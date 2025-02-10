4d91c93a78f12929c520c4e87d8dc6f293b53fc6
Alexander Firsov <gonchaya@gifara.ru>
2025-02-10 15:55:12 +0300

    обновлен установщик под Windows

--- install.bat
+++ install.bat
@@ -1,14 +1,20 @@
-rem cd %HOMEPATH%\Downloads\
-set git_distr=https://github.com/git-for-windows/git/releases/download/v2.47.1.windows.2/Git-2.47.1.2-64-bit.exe
-set el_git=git@github.com:Gatche-clyb/el.git
-set python_installer=https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe
-powershell Invoke-WebRequest %git_distr%  -OutFile .\git_win64.exe
-powershell Invoke-WebRequest %python_installer%  -OutFile .\python_win64.exe
-git_win64.exe
-python_win64.exe /silent
-git clone git@github.com:Gatche-clyb/el.git %HOMEPATH%\el
-cd %HOMEPATH%\el
-pip install colorama
-pip install pandas
-pip install matplotlib
-pip install seaborn
+@echo off
+goto :main
+
+:reload_path
+for /f "tokens=2*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set syspath=%%B
+for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpath=%%B
+set PATH=%PATH%;%syspath%;%userpath%
+powershell "$q='%PATH%'; $w=$q -split ';'| Where-Object {$_.Length -gt 0}| select-object -Unique; $q= $w -join ';'; Set-Content $env:TMP\dfghdfg.txt $q"
+<%TMP%\dfghdfg.txt (set /p PATH=)
+del  %tmp%\dfghdfg.txt
+exit /B 0
+
+:main
+set HOME=%HOMEDRIVE%%HOMEPATH%
+set el_m_install=https://gifara.ru/7/5115.ps1
+powershell Invoke-WebRequest %el_m_install%  -OutFile %HOME%\Downloads\el.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File %HOME%\Downloads\el.ps1
+call :reload_path
+goto :eof
+
