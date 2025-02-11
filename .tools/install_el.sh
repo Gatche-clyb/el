aa72cfd161c6d85ba1d645d9cc79b75452532f11
Alexander Firsov <gonchaya@gifara.ru>
2025-02-11 22:33:28 +0300

    протестирован автоустановщик для Ubuntu

--- .tools/install_el.sh
+++ .tools/install_el.sh
@@ -3,11 +3,12 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y python3 python3.12-venv python3-pip
 sudo apt install -y git
-git clone git@github.com:Gatche-clyb/el.git $HOME/el
+ssh -o StrictHostKeyChecking=no git@github.com
+git clone https://github.com/Gatche-clyb/el.git $HOME/el
 cd el
 python -m venv el
 . el/bin/activate
 pip install colorama
-pip instal pandas
+pip install pandas
 pip install matplotlib
 pip install seaborn
